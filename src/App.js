442a10912a80055c68a40e936223046cc4ef2142
Shubham Bhandari <dev.sb18@gmail.com>
2020-10-11 15:16:17 +0530

    material ui

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import Content from './BaseComponents/Content.js';
 import Viz from './BaseComponents/Viz.js';
 import Form from './BaseComponents/Form.js'
 
-function App(){
-  return(
-    <div>
-      <Navbar />
-      <Content />
-      <Viz />
-      <Form />
-    </div>
-  );
+
+function App() {
+    return (
+        <div>
+            <Navbar/>
+            <Content/>
+            <Viz/>
+            <Form/>
+        </div>
+    );
 }
 
 export default App;
