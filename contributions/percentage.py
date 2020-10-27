6bfbdf30144f76c4088b704f13b39cba71d45a90
Vineet Mishra <73199033+vineet-mi@users.noreply.github.com>
2020-10-27 21:08:11 +0530

    Add files via upload

---
+++ contributions/percentage.py
@@ -0,0 +1,13 @@
+x= float(input("marks = "))
+if (x<25) :
+    print("F")
+elif ((x>=25) & (x<45)):
+    print('E')
+elif ((x>=45) & (x<50)):
+    print('D')
+elif ((x>=50) & (x<60)):
+    print('C')
+elif ((x>=60) & (x<80)):
+    print('B')
+else :    
+    print('A')
